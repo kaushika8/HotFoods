8f4d3f0745e08cc5da5e224af741d84127631da4
kaushikabharathi <kaushika.bharathi@centizen.com>
2024-05-22 09:23:06 +0530

    React and css hotFoods app

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import { images } from "../../constants";
+import "./AboutUs.css";
+
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          The most often used photo is that of your own headshot. It is simple
+          and gives the client a glimpse of who they will be working with. A
+          good portrait to use is one where you are looking straight at the
+          camera. It would have a feeling that you are looking and communicating
+          directly with your client.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          You may want to present yourself as a fun and easy-to-work-with
+          photographer. So, you can use a candid photo of yourself. Or maybe,
+          you can edit your portrait and add some filters, elements, and designs
+          to make it more creative.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
